c98ab5d6bd5a6ae334c3e73804a2d74bbed687e2
mateusz.osiak <mateusz.osiak@lingaro.com>
2017-10-13 15:32:43 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by mateusz.osiak on 10/13/2017.
+ */
+@Controller
+public class IndexController {
+
+    
+}
